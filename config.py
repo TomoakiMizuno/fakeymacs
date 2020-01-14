e184b8087aa8f3c93cfeb7bd3a0c6fdc228bbb75
Tomoaki Mizuno <17838713+TomoakiMizuno@users.noreply.github.com>
2020-01-14 09:15:48 +0900

    misc: 不要になったIME切り替えを削除（ことえりの設定を変更した対応）

--- config.py
+++ config.py
@@ -225,7 +225,7 @@ def configure(keymap):
 
     # IME を切り替えるキーを指定する（複数指定可）
     # toggle_input_method_key = ["C-Yen"]
-    toggle_input_method_key = ["C-Yen", "C-o", "C-Space"]
+    toggle_input_method_key = ["C-Yen", "C-o"]
 
     # C-iキーを Tabキーとして使うかどうかを指定する（True: 使う、False: 使わない）
     use_ctrl_i_as_tab = True
